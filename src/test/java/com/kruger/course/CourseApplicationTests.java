bbd5ea5b87f13da033148679c3fc0e3dd61163eb
Gabriel Kruger <krugergabriel27@outlook.com>
2023-02-27 09:06:40 -0300

    Project created

---
+++ src/test/java/com/kruger/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
