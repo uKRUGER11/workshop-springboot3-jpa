c96ca0e6de5f2a0536e54dff673cc0d0673e5d02
Gabriel Kruger <krugergabriel27@outlook.com>
2023-02-27 16:45:22 -0300

    JPA repository, dependency injection and database seeding

---
+++ src/main/java/com/kruger/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kruger.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kruger.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
