801cb65d7c9e5016c7cd97d39be070642ca31b3c
Gabriel Kruger <krugergabriel27@outlook.com>
2023-02-28 18:12:59 -0300

    Category

---
+++ src/main/java/com/kruger/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kruger.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kruger.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
