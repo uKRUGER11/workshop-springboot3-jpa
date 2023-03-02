c3c92e3eb6e5c9671fd4b515fab771f116e22156
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-02 18:19:00 -0300

    Exception handling - findById

--- src/main/java/com/kruger/course/services/UserService.java
+++ src/main/java/com/kruger/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.kruger.course.entities.User;
 import com.kruger.course.repositories.UserRepository;
+import com.kruger.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj){
